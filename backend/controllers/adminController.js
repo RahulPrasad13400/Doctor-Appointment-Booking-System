5cbc03ee939bc293b8591f99c060eb24c447f5eb
Rahul Prasad <rahulprasad1399@gmail.com>
2025-07-03 23:42:40 +0530

    implementing api logic for doc booking

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -122,4 +122,15 @@ const adminLogin = async (req, res) => {
   }
 };
 
-export { addDoctor, adminLogin };
+// API TO GET ALL DOCTORS FOR ADMIN PANEL 
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select('-password')
+    res.json({success : true, doctors})
+  } catch (error) {
+    console.log(error)
+    res.json({success : false, message : error.message})
+  }
+}
+
+export { addDoctor, adminLogin, allDoctors };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 // APP CONFIG
 const app = express()
@@ -19,6 +21,8 @@ app.use(cors())
 
 // API ENDPOINTS 
 app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res)=>{
     res.send('api working')

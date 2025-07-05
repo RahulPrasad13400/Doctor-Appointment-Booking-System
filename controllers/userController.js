91bcb9eedab40a93a045452544cd0f237d55fd9e
Rahul Prasad <rahulprasad1399@gmail.com>
2025-07-05 22:03:21 +0530

    completed booking and cancellation

---
+++ controllers/userController.js
@@ -0,0 +1,336 @@
+import validator from "validator";
+import bcrypt, { genSalt } from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+import Razorpay from "razorpay";
+
+
+// API TO REGISTER USER
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({
+        success: false,
+        message: "Please enter all the required fields",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Enter a strong password",
+      });
+    }
+
+    // HASHING USER PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//  API FOR USER LOGIN
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Please enter all the required fields",
+      });
+    }
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Email not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      return res.json({
+        success: true,
+        token,
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//  API TO GET USER PROFILE DATA
+const getProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const userData = await userModel.findById(userId).select("-password");
+    res.json({
+      success: true,
+      userData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//  API TO UPDATE USER PROFILE
+const updateProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const { name, phone, address, dob, gender } = req.body;
+    const imageFile = req.file;
+
+    if (!name || !phone || !dob || !gender) {
+      return res.json({
+        success: false,
+        message: "Data missing",
+      });
+    }
+
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    });
+
+    if (imageFile) {
+      // upload image to cloudinary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageUrl = imageUpload.secure_url;
+      await userModel.findByIdAndUpdate(userId, { image: imageUrl });
+    }
+
+    res.json({
+      success: true,
+      message: "Profile updated",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// API TO BOOK APPOINTMENT
+const bookAppointment = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { docId, slotDate, slotTime } = req.body;
+
+    const docData = await doctorModel.findById(docId).select("-password");
+
+    if (!docData.available) {
+      return res.json({
+        success: false,
+        message: "Doctor not available",
+      });
+    }
+
+    let slots_booked = docData.slots_booked;
+
+    // CHECKING FOR SLOT AVAILABILITY
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({
+          success: false,
+          message: "Slot not available",
+        });
+      } else {
+        slots_booked[slotDate].push(slotTime);
+      }
+    } else {
+      // IF YOU ARE THE FIRST ONE TO BOOK ON THAT DATE
+      slots_booked[slotDate] = [];
+      slots_booked[slotDate].push(slotTime);
+    }
+
+    // USER
+    const userData = await userModel.findById(userId).select("-password");
+
+    // IT IS DELETED HERE BECAUSE THE DOC DATA DONT WANT TO CONTAIN ITEMS LIKE SLOTS BOOKED THAT WE PASS AS THE APPOINTMENT DATA TO THE FRONT END
+    delete docData.slots_booked;
+
+    const appointmentData = {
+      userId,
+      docId,
+      userData,
+      docData,
+      amount: docData.fees,
+      slotTime,
+      slotDate,
+      date: Date.now(),
+    };
+
+    const newAppointment = new appointmentModel(appointmentData);
+    await newAppointment.save();
+
+    // save new slots data in docData
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+
+    res.json({
+      success: true,
+      message: "Appointment booked",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// API TO GET APPOINTMENTS FOR MY-APPOINTMENT PAGE
+const listAppointment = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const appointments = await appointmentModel.find({ userId });
+
+    res.json({
+      success: true,
+      appointments,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// API TO CANCEL APPOINTMENTS
+const cancelAppointment = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const {appointmentId} = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    // VERIFY APPOINTMENT USER
+    if (appointmentData.userId !== userId) {
+      return res.json({
+        success: false,
+        message: "Unauthorized action",
+      });
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    // RELEASING DOCTOR SLOT
+    const { docId, slotDate, slotTime } = appointmentData;
+
+    const docData = await doctorModel.findById(docId);
+
+    let slots_booked = docData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (time) => time !== slotTime
+    );
+
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+    res.json({
+      success : true,
+      message : "Appointment cancelled"
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const razorpayInstance = new Razorpay({
+  key_id : '',
+  key_secret : '',
+})
+
+// API TO MAKE PAYMENT USING RAZORPAY
+const paymentRazorpay = async (req, res) => {
+  try {
+    const {appointmentId} = req.body
+    const appointmentData = await appointmentModel.findById(appointmentId)
+    const fee = appointmentData.amount
+
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });    
+  }
+}
+
+export {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+  bookAppointment,
+  listAppointment,
+  cancelAppointment,
+  paymentRazorpay
+};
